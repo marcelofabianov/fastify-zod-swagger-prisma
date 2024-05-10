f667869d80a353bd3945e04bcde45c065e7902cc
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-05-10 07:27:38 -0300

    feat: iniciar containers wrapper

---
+++ src/_core/container/container-wrapper.ts
@@ -0,0 +1,31 @@
+import { ErrorHandle } from '../errors/error-handle'
+
+import { ContainerWrapperInterface } from './container-wrapper-interface'
+
+export class ContainerWrapper implements ContainerWrapperInterface {
+  private readonly container: Record<string, unknown>
+
+  constructor() {
+    this.container = {}
+  }
+
+  add(key: string, value: unknown): void {
+    if (this.has(key)) {
+      throw new ErrorHandle(500, `Key ${key} already exists in container`)
+    }
+
+    this.container[key] = value
+  }
+
+  get(key: string): unknown {
+    if (!this.has(key)) {
+      throw new ErrorHandle(500, `Key ${key} does not exist in container`)
+    }
+
+    return this.container[key]
+  }
+
+  has(key: string): boolean {
+    return this.container[key] !== undefined
+  }
+}
