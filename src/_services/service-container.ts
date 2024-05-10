29aebed72c4a0474e781f2012274dd2f630db5c8
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-05-10 20:30:04 -0300

    feat: criar container ServiceContainer

--- src/_services/service-container.ts
+++ src/_services/service-container.ts
@@ -1,14 +1,27 @@
+import { PasswordValidate } from './password/password-validate'
 import { ContainerInterface } from '@/_core/container/container-interface'
 import { ContainerWrapperInterface } from '@/_core/container/container-wrapper-interface'
-import { HashService } from './hash/hash-service'
 import { env } from '@/_core/environment'
+import { HashService } from './hash/hash-service'
+import { PasswordRandom } from './password/password-random'
+import { PasswordService } from './password/password-service'
 
 export class ServiceContainer implements ContainerInterface {
   public constructor(private container: ContainerWrapperInterface) {}
 
   public register(): void {
-    const hash = new HashService(env.HASH_SALT)
+    const hashSalt = env.HASH_SALT
+    const hashService = new HashService(hashSalt)
+    const passwordValidate = new PasswordValidate()
+    const passwordRandom = new PasswordRandom(passwordValidate)
 
-    this.container.add('HashService', hash)
+    const passwordService = new PasswordService(
+      hashService,
+      passwordRandom,
+      passwordValidate,
+    )
+
+    this.container.add('hashService', hashService)
+    this.container.add('passwordService', passwordService)
   }
 }
