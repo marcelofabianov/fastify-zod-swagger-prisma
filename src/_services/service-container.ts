306d19522f051d34a306e1f68234aa4afabdee8f
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-05-10 19:01:18 -0300

    refactor: atualizar service container com hash service

--- src/_services/service-container.ts
+++ src/_services/service-container.ts
@@ -1,11 +1,14 @@
 import { ContainerInterface } from '@/_core/container/container-interface'
 import { ContainerWrapperInterface } from '@/_core/container/container-wrapper-interface'
 import { HashService } from './hash/hash-service'
+import { env } from '@/_core/environment'
 
 export class ServiceContainer implements ContainerInterface {
   public constructor(private container: ContainerWrapperInterface) {}
 
   public register(): void {
-    this.container.add('HashService', new HashService())
+    const hash = new HashService(env.HASH_SALT)
+
+    this.container.add('HashService', hash)
   }
 }
